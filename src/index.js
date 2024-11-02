11126d00cc9d277f96015a235ce8ba4624ff2fde
andytaotest <andytaotest@gmail.com>
2024-11-02 19:58:11 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Main App Component
+function App() {
+  // Sample data for books
+  const books = [
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      summary: "A story of love, wealth, and tragedy in the 1920s.",
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      summary: "A dystopian novel exploring the dangers of totalitarianism.",
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      summary: "A novel about racial injustice in the American South.",
+    },
+  ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div className="app">
+      <h1>Library Book Display</h1>
+      <div className="book-list"></div>
+    </div>
+  );
+}
