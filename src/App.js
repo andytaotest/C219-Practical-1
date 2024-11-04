148d498aa3fbd829bcf4ae53416e07cf80b4fd9b
andytaotest <andytaotest@gmail.com>
2024-11-04 10:42:37 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,5 @@
 import "./App.css";
 
-// LibraryStatus Component (Displays whether the library is open or closed)
-function LibraryStatus({ isOpen }) {
-  return (
-    <div>
-      {isOpen ? (
-        <>
-          <p>The library is open</p>
-          <button>Visit Library</button>
-        </>
-      ) : (
-        <p>The library is closed</p>
-      )}
-    </div>
-  );
-}
-
-// Book Component (Displays individual book details)
-function Book({ title, author, summary }) {
-  return (
-    <div className="book">
-      <h2>{title}</h2>
-      <p>
-        <strong>Author:</strong> {author}
-      </p>
-      <p>{summary}</p>
-    </div>
-  );
-}
-
 function App() {
   // Sample data for books
   const books = [
@@ -49,28 +20,10 @@ function App() {
     },
   ];
 
-  // Determine if the library is open based on current hour
-  const currentHour = new Date().getHours();
-  const isOpen = currentHour >= 9 && currentHour < 18;
-
   return (
     <div className="app">
       <h1>Library Book Display</h1>
-
-      {/* LibraryStatus component to show open/closed status */}
-      <LibraryStatus isOpen={isOpen} />
-
-      <div className="book-list">
-        {/* Render each book using the Book component */}
-        {books.map((book, index) => (
-          <Book
-            key={index}
-            title={book.title}
-            author={book.author}
-            summary={book.summary}
-          />
-        ))}
-      </div>
+      <div className="book-list"></div>
     </div>
   );
 }
